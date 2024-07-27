a17e2270209471205496bc48d84af03aacbfb03d
Cidmar Oliveira <93956777+ciiDmo@users.noreply.github.com>
2024-07-27 11:34:20 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+  basePath: isProd ? '/lunch-wheel' : '',
   reactStrictMode: true,
   output: 'export',
   distDir: 'dist',
